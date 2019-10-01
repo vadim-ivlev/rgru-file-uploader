aa0af0ed98dba6fa4e44e559437e8fca2b90b956
Ивлев Вадим <ivlev@rg.ru>
2019-10-01 15:23:53 +0300

    fix: CORS Access-Control-Allow-Origin

--- server/middleware.go
+++ server/middleware.go
@@ -10,7 +10,7 @@ import (
 func HeadersMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-		c.Header("Access-Control-Allow-Origin", "*")
+		// c.Header("Access-Control-Allow-Origin", "*")
 		// if hostIsAllowed(c.Request.Host) {
 		// 	c.Header("Access-Control-Allow-Origin", "*")
 		// }
