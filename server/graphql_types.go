7f863054f7ed6a2277e8259b9a3eb4dd98a5f96d
Ивлев Вадим <ivlev@rg.ru>
2020-01-14 19:11:47 +0300

    add: graphql types DominantColor and CropRect

--- server/graphql_types.go
+++ server/graphql_types.go
@@ -6,6 +6,21 @@ import (
 
 // TYPES ****************************************************
 
+var dominantColorType = graphql.NewObject(graphql.ObjectConfig{
+	Name:        "DominantColor",
+	Description: "Dominant color in the image",
+	Fields: graphql.Fields{
+		"hex": &graphql.Field{
+			Type:        graphql.String,
+			Description: "Hex representation of the color",
+		},
+		"is_light": &graphql.Field{
+			Type:        graphql.Boolean,
+			Description: "If the color looks light",
+		},
+	},
+})
+
 var imageType = graphql.NewObject(graphql.ObjectConfig{
 	Name:        "File",
 	Description: "File.",
@@ -13,6 +28,10 @@ var imageType = graphql.NewObject(graphql.ObjectConfig{
 		"filepath": &graphql.Field{
 			Type:        graphql.String,
 			Description: "File URI",
+		},
+		"ext": &graphql.Field{
+			Type:        graphql.String,
+			Description: "Filename extension",
 		},
 		"width": &graphql.Field{
 			Type:        graphql.Int,
@@ -29,6 +48,33 @@ var imageType = graphql.NewObject(graphql.ObjectConfig{
 		"initial_size": &graphql.Field{
 			Type:        graphql.Int,
 			Description: "Initial file size in bytes",
+		},
+		"dominant_color": &graphql.Field{
+			Type:        dominantColorType,
+			Description: "Dominant color in the image",
+		},
+	},
+})
+
+var rectType = graphql.NewObject(graphql.ObjectConfig{
+	Name:        "Rect",
+	Description: "Rectangular area on the image",
+	Fields: graphql.Fields{
+		"x": &graphql.Field{
+			Type:        graphql.Int,
+			Description: "distance between LEFT sides of the image and the crop rectangle in pixels",
+		},
+		"y": &graphql.Field{
+			Type:        graphql.Int,
+			Description: "distance between TOP sides of the image and the crop rectangle in pixels",
+		},
+		"width": &graphql.Field{
+			Type:        graphql.Int,
+			Description: "width of the rectangle in pixels",
+		},
+		"height": &graphql.Field{
+			Type:        graphql.Int,
+			Description: "height of the rectangle in pixels",
 		},
 	},
 })
